ae1c5c700292f2ed12681cd24f79ea8a0a08dce6
Daniel Mai <daniel@udacity.com>
2015-07-19 14:43:55 -0700

    Add launcher icon and OCR parser logic.

---
+++ app/src/main/java/com/foodlabelapp/foodlabel/models/NutriFactList.java
@@ -0,0 +1,60 @@
+package com.foodlabelapp.foodlabel.models;
+
+public class NutriFactList {
+
+  private static NutriFactList sNutriFactList;
+  private NutriFact[] list;
+  private int curr;
+  private int size;
+  private int totalCal;
+  private int totalFat;
+  private int totalSodium;
+  private int totalCarbs;
+
+  // Creates a NutriFactList of size
+  public NutriFactList (int size) {
+    list = new NutriFact[size];
+    this.size = size;
+    curr = 0;
+    totalCal = 0;
+  }
+
+  public void addNutriFact (String str) {
+    if (curr < size) {
+      list[curr] = new NutriFact(str);
+      totalCal += list[curr].getCalories();
+      totalFat += list[curr].getTotalFat();
+      totalSodium += list[curr].getSodium();
+      totalCarbs += list[curr].getCarbs();
+      curr++;
+    }
+  }
+
+  public NutriFact getNutriFact (int index) {
+    return list[index];
+  }
+
+  public int getCalories () {
+    return totalCal;
+  }
+
+  public int getTotalFat () {
+    return totalFat;
+  }
+
+  public int getSodium () {
+    return totalSodium;
+  }
+
+  public int getCarbs () {
+    return totalCarbs;
+  }
+
+  public static NutriFactList getInstance() {
+    if (sNutriFactList == null) {
+      sNutriFactList = new NutriFactList(2000);
+    }
+    return sNutriFactList;
+  }
+
+}
