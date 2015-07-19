baf29f6678cb8038a3d7e0ecf12f58935cb70cfd
Daniel Mai <daniel@udacity.com>
2015-07-19 00:47:15 -0700

    Clean up unused imports.

--- app/src/main/java/com/foodlabelapp/foodlabel/MainActivity.java
+++ app/src/main/java/com/foodlabelapp/foodlabel/MainActivity.java
@@ -1,8 +1,6 @@
 package com.foodlabelapp.foodlabel;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
